319c813ab5bf9e874bbabe6218e1d6eef380d035
ppooopppooo <ppooopppooo@qq.com>
2021-11-07 22:11:18 +0800

    libbinder: use the same libbinder header for camera
     * make camera lag when open
    
    Change-Id: I1b8437a0b2b7c6daac49acf7124d8f3cdc428440

--- libs/binder/Parcel.cpp
+++ libs/binder/Parcel.cpp
@@ -592,10 +592,10 @@ void Parcel::updateWorkSourceRequestHeaderPosition() const {
     }
 }
 
-#if defined(__ANDROID_VNDK__)
-constexpr int32_t kHeader = B_PACK_CHARS('V', 'N', 'D', 'R');
-#else
+#if defined(LIBBINDER_HAS_THE_SAME_HEADER) || !defined(__ANDROID_VNDK__)
 constexpr int32_t kHeader = B_PACK_CHARS('S', 'Y', 'S', 'T');
+#else
+constexpr int32_t kHeader = B_PACK_CHARS('V', 'N', 'D', 'R');
 #endif
 
 // Write RPC headers.  (previously just the interface token)
